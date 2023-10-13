8e158d95bac7e97723db5dc29d7cceefb19a4083
Mahdi <mahdisedaghatzadeh@gmail.com>
2023-10-13 14:29:01 +0330

    add animation to skills

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+const fullBoxAnimation = () => {
+  let skillBox = document.querySelector(".skills");
+  let windowHeight = window.innerHeight;
+  let skillBoxTop = skillBox.getBoundingClientRect().top;
+  let fullPoint = 50;
+  skillBoxTop < windowHeight - fullPoint ? skillBox.classList.add("active") : "";
+};
+
+window.addEventListener("scroll", ()=> {
+    fullBoxAnimation()
+})
