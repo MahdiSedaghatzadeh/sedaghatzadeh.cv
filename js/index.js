ef0129fba0740b11db395e57bfcb0a60e8163083
Mahdi <mahdisedaghatzadeh@gmail.com>
2023-10-13 15:56:31 +0330

    update styles

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
-
 const fullBoxAnimation = () => {
   let skillBox = document.querySelector(".skills");
   let windowHeight = window.innerHeight;
   let skillBoxTop = skillBox.getBoundingClientRect().top;
   let fullPoint = 50;
-  skillBoxTop < windowHeight - fullPoint ? skillBox.classList.add("active") : "";
+  skillBoxTop < windowHeight - fullPoint
+    ? skillBox.classList.add("active")
+    : "";
 };
 
-window.addEventListener("scroll", ()=> {
-    fullBoxAnimation()
-})
+window.addEventListener("scroll", () => {
+  fullBoxAnimation();
+});
+
+document.body.addEventListener("touchmove", () => {
+  fullBoxAnimation();
+});
