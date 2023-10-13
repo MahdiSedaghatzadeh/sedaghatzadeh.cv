2f7c7f88dd25248c0fe885fea4be118d8c4d052b
Mahdi <mahdisedaghatzadeh@gmail.com>
2023-10-13 16:13:59 +0330

    test

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const fullBoxAnimation = () => {
+    console.log("it is working");
   let skillBox = document.querySelector(".skills");
   let windowHeight = window.innerHeight;
   let skillBoxTop = skillBox.getBoundingClientRect().top;
-  let fullPoint = 50;
-  skillBoxTop < windowHeight - fullPoint
+  skillBoxTop <= windowHeight 
     ? skillBox.classList.add("active")
-    : "";
+    : ""
 };
 
 window.addEventListener("scroll", () => {
